1888c3f320f34c016aeda90b4418eabd20e459d3
Justin <jjasa.2000@gmail.com>
2022-01-28 12:15:03 +1100

    added sneakerdon headtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Layout from '../components/Layout/Layout';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
+			<Head>
+				<title>SneakerDon</title>
+				<meta
+					name="SneakerDon"
+					content="An application with tracks sneaker prices"
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</Layout>
 	);
