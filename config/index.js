582ff1a092db83b74a385528cba6f8eb4387b6ec
Justin <jjasa.2000@gmail.com>
2022-03-01 08:58:07 +1100

    small changes

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 export const server =
-	'http://localhost:8080' || 'https://sneakerdon.herokuapp.com';
+	'https://sneakerdon.herokuapp.com' || 'http://localhost:8080';
 
 export const client = 'http://localhost:3000';
